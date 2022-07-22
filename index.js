9a542a49df6b3612acfc329e5ce9443d0d551765
macarenamon <makiimon@hotmail.com>
2022-07-21 21:39:55 -0300

    eventos

--- index.js
+++ index.js
@@ -1,36 +1,81 @@
-class Servicio {
-    constructor(tipo, vehiculo, precio) {
-        this.tipo = tipo;
-        this.vehiculo = vehiculo;
-        this.precio = precio;
-    }
-}
-const servicios = [
-    new Servicio('Practico', 'Auto', 2000 ),
-    new Servicio('Teorico', 'No requiere automovil', 2500),
-    new Servicio('de Estacionamiento', 'Vehiculo a eleccion', 2000)
-]
+// class Servicio {
+//     constructor(tipo, vehiculo, precio) {
+//         this.tipo = tipo;
+//         this.vehiculo = vehiculo;
+//         this.precio = precio;
+//     }
+// }
+// const servicios = [
+//     new Servicio('Practico', 'Auto', 2000 ),
+//     new Servicio('Teorico', 'No requiere automovil', 2500),
+//     new Servicio('de Estacionamiento', 'Vehiculo a eleccion', 2000)
+// ]
 
-function mostrarServicios() {
-    alert('Nuestra empresa cuenta con los siguiente cursos:');
-    for (const servicio of servicios) {
-        alert(`Curso ${servicio.tipo}`);
-    }
+// function mostrarServicios() {
+//     alert('Nuestra empresa cuenta con los siguiente cursos:');
+//     for (const servicio of servicios) {
+//         alert(`Curso ${servicio.tipo}`);
+//     }
 
-    pedirDatos();
-}
+//     pedirDatos();
+// }
 
-function pedirDatos() {
-    const datoUsuario = prompt('¿Qué curso desea contratar?');
-    const servicioEncontrado = servicios.find(servicio => servicio.tipo.toLowerCase() === datoUsuario.toLowerCase());
+// function pedirDatos() {
+//     const datoUsuario = prompt('¿Qué curso desea contratar?');
+//     const servicioEncontrado = servicios.find(servicio => servicio.tipo.toLowerCase() === datoUsuario.toLowerCase());
 
-    if (servicioEncontrado) {
-        alert(`El curso ${servicioEncontrado.tipo} tiene un costo de $${servicioEncontrado.precio}`)
+//     if (servicioEncontrado) {
+//         alert(`El curso ${servicioEncontrado.tipo} tiene un costo de $${servicioEncontrado.precio}`)
+//     } else {
+//         alert('No contamos con ese curso, inténtelo nuevamente');
+//         mostrarServicios();
+//     }
+
+// }
+
+// mostrarServicios();
+
+const formulario = document.querySelector('#my-form')
+const inputNombre = document.querySelector('#input-nombre')
+const inputMail = document.querySelector('#input-mail')
+const inputCurso = document.querySelector('#input-curso')
+const btnEnviar = document.querySelector('#btn-enviar')
+
+inputNombre.addEventListener('keydown', (event) => {
+
+   
+    if (inputNombre.value.length <= 4) {
+        inputNombre.classList.add('border-danger')
+        inputNombre.classList.remove('border-success')
     } else {
-        alert('No contamos con ese curso, inténtelo nuevamente');
-        mostrarServicios();
+        inputNombre.classList.add('border-success')
+        inputNombre.classList.remove('border-danger')
+    }
+})
+
+inputMail.addEventListener('input', () => {
+
+
+    if (inputMail.value.length <= 8) {
+        inputMail.classList.add('border-danger')
+        inputMail.classList.remove('border-success')
+    } else {
+        inputMail.classList.add('border-success')
+        inputMail.classList.remove('border-danger')
+    }
+})
+
+
+formulario.addEventListener('submit', (e) => {
+    e.preventDefault()
+    console.log(e)
+    
+    const userData = {
+        nombre: inputNombre.value,
+        mail: inputMail.value,
+        curso: inputCurso.value
     }
 
-}
+    console.log(userData)
+})
 
-mostrarServicios();
